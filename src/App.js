d0d6d30b90f34c49929f839b830eeeffbb141d55
Md Abbas Ali <mdabbasali185@gmail.com>
2022-05-22 20:42:47 +0600

    component create

--- src/App.js
+++ src/App.js
@@ -6,18 +6,30 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from './Firebase/firebase.init';
 import RequireAuth from "./Auth/RequireAuth/RequireAuth"
 import './App.css';
+import Home from './Pages/Home/Home';
+import Header from './Pages/Shared/Header/Header';
+import Blogs from './Pages/Blogs/Blogs';
+import './App.css';
 import LogIn from './Auth/LogIn/LogIn';
 import SignUp from './Auth/SignUp/SignUP';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
+import Footer from './Pages/Shared/Footer/Footer';
+import NotFound from './Pages/Shared/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <header />
+      <Header />
       <Routes>
-        <Route path='/' element={<LogIn />}></Route>
+      <Route path='/' element={<Home />}></Route>
         <Route path='/login' element={<LogIn />}></Route>
         <Route path='/signup' element={<SignUp />}></Route>
+        <Route path='/blogs' element={<Blogs />}></Route>
+        <Route path="*" element={<NotFound />} />
       </Routes>
+      <Footer />
+      <ToastContainer></ToastContainer>
     </div>
   );
 }
---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,68 @@
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Card, Carousel, Col, Row } from 'react-bootstrap';
+import mobile from '../../Images/Samsung-Galaxy-J3-LCD-Screen-Display-and-Touch-Panel-Digitizer-Assembly-Replacement-1.jpg'
+import './Home.css';
+
+
+const Home = () => {
+
+    const [loading, setLoading] = useState(true)
+
+
+
+
+
+    if (loading) {
+        return <div className='text-center d-flex align-items-center text-danger justify-content-center' style={{ height: '100vh' }}> <FontAwesomeIcon icon={faSpinner} className='fa-pulse fa-10x'></FontAwesomeIcon> </div>
+    }
+
+
+    return (
+        <div>
+            <div>
+                <Carousel fade>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100 image"
+                            src={mobile}
+                            alt="First slide"
+                        />
+                        <Carousel.Caption>
+                            <h1>Designed with precision</h1>
+                            <p>The quicker, the better.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100 image"
+                            src={mobile}
+                            alt="Second slide"
+                        />
+
+                        <Carousel.Caption>
+                            <h1>Made affordable for you</h1>
+                            <p>Your office companion.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100 image"
+                            src={mobile}
+                            alt="Third slide"
+                        />
+
+                        <Carousel.Caption>
+                            <h1>IT experiences made better</h1>
+                            <p>Let your fingers do the talking.</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
+            </div>
+        </div >
+    );
+};
+
+export default Home;
